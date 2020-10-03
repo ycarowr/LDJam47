fff775148fac00e14f65471c350ca69b53897ff2
alexandresantana <alexandre.limas.santana@gmail.com>
2020-10-03 22:00:37 +0200

    Ready for tutorial step testing

---
+++ Assets/Scripts/Tutorial/TutorialStep.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Tutorial
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class TutorialStep : MonoBehaviour
+    {
+        public TutorialStep nextStep;
+        public bool isFirst;
+
+        private void Awake()
+        {
+            if(!isFirst)
+                gameObject.SetActive(false);
+        }
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(nextStep != null)
+                nextStep.gameObject.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
