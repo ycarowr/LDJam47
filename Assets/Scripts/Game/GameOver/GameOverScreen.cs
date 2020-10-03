c12243b082f53245bd0c7573dd3132e3cfabd32f
Ycaro Weschenfelder Rodrigues <ywrycaro@gmail.com>
2020-10-03 11:56:12 +0300

    Add game flow scene structures

---
+++ Assets/Scripts/Game/GameOver/GameOverScreen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverScreen : MonoBehaviour
+{
+    [SerializeField] private GameObject content;
+    [SerializeField] private Button buttonContinue;
+    [SerializeField] private string nextScene;
+
+    private void Awake()
+    {
+        buttonContinue.onClick.AddListener(OnPressContinue);
+        Hide();
+    }
+
+    private void OnPressContinue()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void Show()
+    {
+        content.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        content.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Menu/ButtonPlayMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonPlayMenu : MonoBehaviour
+{
+    [SerializeField] private string nextScene;
+
+
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
