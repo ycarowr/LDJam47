a6f9b49fe086abf709bd957f3f5af089ffc36245
Ycaro Weschenfelder Rodrigues <ywrycaro@gmail.com>
2020-10-03 13:10:30 +0300

    Add mini tutorial obj

---
+++ Assets/Scripts/MiniTutorial/MiniTutorial.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniTutorial : MonoBehaviour
+{
+    [SerializeField] private GameObject content;
+    
+    public static MiniTutorial Get()
+    {
+        return FindObjectOfType<MiniTutorial>();
+    }
+
+    public void Show()
+    {
+        content.SetActive(true);    
+    }
+
+    public void Hide()
+    {
+        content.SetActive(false);
+    }
+}
